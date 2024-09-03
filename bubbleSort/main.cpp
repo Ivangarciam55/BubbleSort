860f13b0ce5b665abc19cc53d15271397629c6d5
Ivangarciam55 <a22110076@ceti.mx>
2024-09-03 11:20:27 -0600

    Se le agregó la opción de que imprimiera cada vuelta, por ende ya muestra cada intercambio del metodo

--- bubbleSort/main.cpp
+++ bubbleSort/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void bSort();
 unsigned t0, t1;
+int contador = 1;
 
 
 int main() {
@@ -25,7 +26,7 @@ void bSort() {
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(1, 99999999);
+    uniform_int_distribution<> dis(1, 50);
 
     cout << "Desordenado" << endl;
 
@@ -36,17 +37,21 @@ void bSort() {
         cout << num[i] << " ";
     }
 
-    cout << endl << "Ordenado" << endl;
     for(int i = 0; i < tam; i++){
         for(int j = 0; j < tam-i-1; j++) {
             if(num[j] > num[j +1]) {
+                cout << endl <<"Vuelta " <<contador++ << " ";
                 const int pos = num[j];
+                cout << pos << " ";
                 num[j] = num[j +1];
+                cout << num[j] << " ";
                 num[j +1] = pos;
+                cout << num[j +1] << " ";
             }
+
         }
     }
-
+    cout << endl << "Ordenado" << endl;
     for(int i = 0; i < tam; i++) {
         cout << num[i] << " ";
     }
